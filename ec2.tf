730decc1119ed0a702abd03e60f977fbe537e019
OMPRASAD <ommprasad24@gmail.com>
2024-01-09 17:17:31 +0530

    changing  new lines of code in tfvars file

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "aws_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   count         = var.instance_count
+  
 }
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.mybucket
